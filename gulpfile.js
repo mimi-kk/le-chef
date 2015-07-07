65265fe06712ae1db1e0bd29c2a6754467e66bff
Min Jeong Kim Kühne <minjeong.kimkuehne@tages-anzeiger.ch>
2015-07-07 19:48:37 +0200

    Integrated Browser-Sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require("gulp");
+var browserSync = require("browser-sync");
 
 gulp.task("default", function() {
-	console.log("Default task!");
+    console.log("Default task!");
+});
+
+gulp.task("server", function() {
+    browserSync.init({
+        server: "client",
+        notify: false
+    });
 });
