caf2c4de438af95a41c8e6f6fc48c5867b8f032e
Nora Rusterholz <norarusterholz@gmail.com>
2015-07-30 11:06:05 +0200

    Added some recipes

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -23,12 +23,152 @@
     videos: [],
     mass: [{
         us: {
-            "fahrenheit": "",
-            "teaspoon": "",
-            "tablespoon": "",
-            "cup": "",
-            "ounce": "",
-            "pound": ""
+            fahrenheit: "",
+            teaspoon: "",
+            tablespoon: "",
+            cup: "",
+            ounce: "",
+            pound: ""
+        }
+    }, {
+        metric: {
+            g: "",
+            kg: "",
+            ml: "",
+            l: "",
+            celsius: ""
+        }
+    }],
+    rating: [],
+  }, {
+    id: "1",
+    date: "",
+    author: "Ella",
+    title: "Porridge",
+    lead: "Blueberries ‘n’ Cream Amaranth",
+    readytime: [{
+        preptime: ""
+    }, {
+        cooktime: ""
+    }],
+    instructions: "Drain and rinse. Bring to boild and let simmer for 15 minutes",
+    keywords: ["Glutenfree", "Healthy", "Amaranth"],
+    ingredients: ["Amaranth", "Water", "Blueberries"],
+    images: "../images/amaranth-200x200px.jpg",
+    videos: [],
+    mass: [{
+        us: {
+            fahrenheit: "",
+            teaspoon: "",
+            tablespoon: "",
+            cup: "",
+            ounce: "",
+            pound: ""
+        }
+    }, {
+        metric: {
+            g: "",
+            kg: "",
+            ml: "",
+            l: "",
+            celsius: ""
+        }
+    }],
+    rating: [],
+  }, {
+    id: "1",
+    date: "",
+    author: "Ella",
+    title: "Pizza",
+    lead: "Baby spinach 'n' red onions",
+    readytime: [{
+        preptime: ""
+    }, {
+        cooktime: ""
+    }],
+    instructions: "Make dough. Roll the dough out onto a surface covered in cornmeal",
+    keywords: ["Pizza", "Healthy", "Italian"],
+    ingredients: ["Spinach", "Onions", "Flour"],
+    images: "../images/pizza-200x200px.jpg",
+    videos: [],
+    mass: [{
+        us: {
+            fahrenheit: "",
+            teaspoon: "",
+            tablespoon: "",
+            cup: "",
+            ounce: "",
+            pound: ""
+        }
+    }, {
+        metric: {
+            g: "",
+            kg: "",
+            ml: "",
+            l: "",
+            celsius: ""
+        }
+    }],
+    rating: [],
+  }, {
+    id: "1",
+    date: "",
+    author: "Ella",
+    title: "Carrot Soup",
+    lead: "Curry 'n' coconut",
+    readytime: [{
+        preptime: ""
+    }, {
+        cooktime: ""
+    }],
+    instructions: "Heat olive oil over medium heat. Cook for 3-4 minutesd and let simmer for 15 minutes.",
+    keywords: ["Glutenfree", "Healthy", "Summer"],
+    ingredients: ["Carrots", "Water", "Coconut milk"],
+    images: "../images/carrot-200x200px.jpg",
+    videos: [],
+    mass: [{
+        us: {
+            fahrenheit: "",
+            teaspoon: "",
+            tablespoon: "",
+            cup: "",
+            ounce: "",
+            pound: ""
+        }
+    }, {
+        metric: {
+            g: "",
+            kg: "",
+            ml: "",
+            l: "",
+            celsius: ""
+        }
+    }],
+    rating: [],
+  }, {
+    id: "1",
+    date: "",
+    author: "Ella",
+    title: "Tacos",
+    lead: "Avocado and Microgreens",
+    readytime: [{
+        preptime: ""
+    }, {
+        cooktime: ""
+    }],
+    instructions: "Drain and rinse. Bring to boild and let simmer for 15 minutes",
+    keywords: ["Glutenfree", "Healthy", "Mexican"],
+    ingredients: ["Lentils", "Greens", "Corn meal"],
+    images: "../images/tacos-200x200px.jpg",
+    videos: [],
+    mass: [{
+        us: {
+            fahrenheit: "",
+            teaspoon: "",
+            tablespoon: "",
+            cup: "",
+            ounce: "",
+            pound: ""
         }
     }, {
         metric: {
