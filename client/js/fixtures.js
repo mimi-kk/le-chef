1c71a7697d1f40284c82cfd7b64ace789b4af2c8
Nora Rusterholz <norarusterholz@gmail.com>
2015-09-14 14:57:01 +0200

    Added more recipe data

--- client/js/fixtures.js
+++ client/js/fixtures.js
@@ -1,19 +1,22 @@
 window.FIXTURES = [{
     id: "0",
-    date: "",
+    date: "17.06.2015",
     author: "Ella",
     title: "Polenta",
     lead: "Curry Grilled Vegetables with Chickpeas",
     times: [{
-        preptime: "0.5"
+        preptime: "20"
     }, {
-        cooktime: "1.0"
+        cooktime: "30"
     }],
-    instructions: "Drain and rinse. Bring to boild and let simmer for 15 minutes",
+    instructions: "Bring the water, broth and salt to a boil. Stir in polenta; cook until polenta begins to thicken. Cover and reduce heat to lowest setting. Simmer polenta for 25 to 30 minutes, stirring and scraping the pan bottom and sides every 8 to 10 minutes. Remove from heat and stir in butter. Preheat grill. Slice the zucchini and pepper into strips; toss with oil and curry powder. Grill until zucchini and peppers are slightly charred, 3 to 5 minutes per side. Roughly chop the vegetables and toss them with the chickpeas. Divide polenta among individual serving bowls. Top with grilled vegetables, cilantro and feta.",
     keywords: [
         "Glutenfree",
         "Healthy",
-        "Corn"
+        "Corn",
+        "Main",
+        "Dinner",
+        "Lunch"
     ],
     ingredients: [ 
         "2 cups water", 
@@ -63,25 +66,31 @@ window.FIXTURES = [{
     } ],
 }, {
     id: "1",
-    date: "",
+    date: "18.06.2015",
     author: "Ella",
     title: "Porridge",
     lead: "Blueberries ‘n’ Cream Amaranth",
     times: [{
-        preptime: ""
+        preptime: "5"
     }, {
-        cooktime: ""
+        cooktime: "25"
     }],
-    instructions: "Drain and rinse. Bring to boild and let simmer for 15 minutes",
+    instructions: "Soak Amaranth over night (if possible). Drain and rinse amaranth. Combine with one cup water and pinch of salt. Bring amaranth to a boil, and reduce to a simmer (all the way to low.) Cover and let simmer for 15 minutes. Remove from heat and let sit for 10 more minutes to thicken amaranth. In a separate pot add blueberries, maple syrup, and 1 tablespoon of heavy cream. Scrap out vanilla bean and toss both the seeds and the pod into the blueberries. Heat over low until blueberries are heated through (making sure not to boil the heavy cream.) Serve by pouring blueberry liquid in the bottom, followed by amaranth, then top with blueberries.",
     keywords: [
         "Glutenfree",
         "Healthy",
+        "Breakfast",
         "Amaranth"
     ],
-    ingredients: [
-        "Amaranth",
-        "Water",
-        "Blueberries"
+    ingredients: [ 
+        "1/2 cup amaranth, uncooked", 
+        "1 cup water (or coconut milk)", 
+        "1/4 teasoon sea salt", 
+        "1 cup blueberries, frozen or fresh",
+        "1 tablespoon maple syrup",
+        "1/2 vanilla bean",
+        "1-2 tablespoons heavy cream (or coconut milk)",
+        "pinch of salt",
     ],
     thumbail: "../images/amaranth-200x200px.jpg",
     slides: [
@@ -117,25 +126,34 @@ window.FIXTURES = [{
     } ],
 }, {
     id: "2",
-    date: "",
+    date: "19.06.2015",
     author: "Ella",
     title: "Pizza",
     lead: "Baby spinach 'n' red onions",
     times: [{
-        preptime: ""
+        preptime: "20"
     }, {
-        cooktime: ""
+        cooktime: "15"
     }],
-    instructions: "Make dough. Roll the dough out onto a surface covered in cornmeal",
+    instructions: "In a bowl of a stand mixer, combine the water, yeast, and honey- stir with a wooden spoon until yeast is dissolved. Let sit until yeast begins to activate, 5-10 minutes. Once yeast is ready, add in oil, salt, and 1 cup of flour. Turn your mixer on low with the dough hook on and let mix, scrapping down the sides as needed. From here on out, add 2-4 tablespoons of flour at a time, letting the dough mix for a bit until adding more flour. When the dough starts to come together, continue to let the mixer run for 1-2 minutes and adding more flour until your dough has pulled away from the sides of the mixer. Remove from the mixer and place in a warm spot. Brush with oil and let rise for 1 hour. In a small bowl, stir together all ingredients for the ranch dressing except for the buttermilk. Taste and adjust the ranch flavors if need be. Stir in buttermilk, 1 tablespoon at a time, as needed to thin out the ranch if desired. Once your dough has double in size, preheat your oven to 500˚. Return to your dough and punch it down. Let rise for 20 more minutes. Roll the dough out onto a surface covered in cornmeal (I used a large wood cutting board put some of you may have a pizza peel.) Roll it out to the size of your pizza stone. Spread enough ranch over pizza to cover (you may not use all). Cut onion into thin slices and layer over ranch, followed by the spinach. Top pizza with enough cheese to cover most of the spinach. Slide pizza onto stone and let bake until crust and mozzarella is golden brown, 12-15 minutes. Let sit for a few minutes before slicing.",
     keywords: [
         "Pizza",
         "Healthy",
-        "Italian"
+        "Italian",
+        "Main",
+        "Dinner",
+        "Lunch"
     ],
     ingredients: [
-        "Spinach",
-        "Onions",
-        "Flour"
+        "3/4 cup warm water",
+        "2 1/2 teaspoon yeast",
+        "1/4 cup honey",
+        "2 tablespoons olive oil",
+        "1/2teaspoons salt",
+        "1 1/2 - 2 cups whole wheat flour (bread or pastry works well)",
+        "1 small red onion",
+        "3-4 handfuls baby spinach",
+        "2-3 cups shredded mozzarella cheese"
     ],
     thumbail: "../images/pizza-200x200px.jpg",
     slides: [
@@ -171,25 +189,32 @@ window.FIXTURES = [{
     } ],
 }, {
     id: "3",
-    date: "",
+    date: "20.06.2015",
     author: "Ella",
     title: "Carrot Soup",
     lead: "Curry 'n' coconut",
     times: [{
-        preptime: ""
+        preptime: "5"
     }, {
-        cooktime: ""
+        cooktime: "25"
     }],
-    instructions: "Heat olive oil over medium heat. Cook for 3-4 minutesd and let simmer for 15 minutes.",
+    instructions: "In a large pot, heat olive oil over medium heat. Finely dice green onions and carrots, throw into pot, and let cook for 3-4 minutes or until carrots begin to soften. Next, add lentils, curry powder, ginger, and chili powder. Stir and let cook until spices become fragrant, 1-2 minutes. Stir in veggie broth and bring to a boil then reduce to a simmer, cooking until lentils are soft, 10-15 minutes. Remove from heat and using either an immersion blender or a regular blender, puree until soup is smooth. Return soup back to stove, stir in coconut milk, and heat soup for another two minutes, tasting and adding spices as you like.",
     keywords: [
         "Glutenfree",
         "Healthy",
-        "Summer"
+        "Soup",
+        "Winter"
     ],
     ingredients: [
-        "Carrots",
-        "Water",
-        "Coconut milk"
+        "1 tablespoon olive oil",
+        "1/4 cup green onions",
+        "2 cups carrots (4-5 large carrots)",
+        "1/2 cup red lentils",
+        "2 teaspoons curry powder",
+        "1 teaspoon fresh ginger, minced",
+        "1/4 teaspoon chili powder",
+        "3 cups veggie broth",
+        "1/2 cup coconut milk"
     ],
     thumbail: "../images/carrot-200x200px.jpg",
     slides: [
@@ -225,25 +250,36 @@ window.FIXTURES = [{
     } ],
 }, {
     id: "4",
-    date: "",
+    date: "21.06.2015",
     author: "Ella",
     title: "Tacos",
     lead: "Avocado and Microgreens",
     times: [{
-        preptime: ""
+        preptime: "15"
     }, {
-        cooktime: ""
+        cooktime: "20"
     }],
-    instructions: "Drain and rinse. Bring to boild and let simmer for 15 minutes",
+    instructions: "Rinse the lentils. Place them in a sauce pan with the broth, minced chipotle, 1 tablespoon adobo sauce, lime juice, salt, cumin and coriander. Bring to a boil, then reduce to a simmer; cover and cook until lentils are tender but not mushy, 22 to 25 minutes. Taste and add more adobo sauce, if you'd like more heat. Combine the avocado and microgreens. In a small bowl, beat together lime juice, honey and olive oil. Pour over the microgreens and avocado, tossing until greens are coated. Divide the lentil mixture among the tortillas. Top with the avocado salad, crumbled cheese and minced cilantro.",
     keywords: [
         "Glutenfree",
         "Healthy",
         "Mexican"
     ],
     ingredients: [
-        "Lentils",
-        "Greens",
-        "Corn meal"
+        "1/2 cup black lentils",
+        "1 1/2 cups vegetable broth",
+        "2 teaspoons lime juice",
+        "1/4 teaspoon salt",
+        "1/2 teaspoon cumin",
+        "1/2 teaspoon coriander",
+        "1/2 avocado, cut into small cubes",
+        "1 cup mixed microgreens",
+        "1 teaspoon fresh lime juice",
+        "1/2 teaspoon honey",
+        "1 teaspoon olive oil",
+        "4 to 6 corn tortillas",
+        "1 to 2 ounces cotija, feta or goat cheese",
+        "Minced cilantro, garnish"
     ],
     thumbail: "../images/tacos-200x200px.jpg",
     slides: [
