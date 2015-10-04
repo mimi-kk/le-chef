0cb0325809aa8743a629b07737c68adc63c2a461
Min Jeong Kim Kühne <minjeong.kimkuehne@tages-anzeiger.ch>
2015-10-04 15:54:49 +0200

    Added recipe object in EditRecipeController to initialize

--- client/js/controllers/recipe-controller.js
+++ client/js/controllers/recipe-controller.js
@@ -8,114 +8,117 @@
 // });
 
 app.factory("createRecipes", ["$firebaseArray",
-   function($firebaseArray) {
-     var ref = new Firebase("https://fiery-inferno-8595.firebaseio.com/recipes/");
-     return $firebaseArray(ref);
-   }
+  function($firebaseArray) {
+    var ref = new Firebase("https://fiery-inferno-8595.firebaseio.com/recipes/");
+    return $firebaseArray(ref);
+  }
 ]);
 
-app.controller("RecipesController", [ "$scope", "$firebaseArray", 
-    function($scope, $firebaseArray) {
-        var ref = new Firebase("https://fiery-inferno-8595.firebaseio.com/recipes/");
-        $scope.recipes = $firebaseArray(ref);
-    }
+app.controller("RecipesController", ["$scope", "$firebaseArray", 
+  function($scope, $firebaseArray) {
+    var ref = new Firebase("https://fiery-inferno-8595.firebaseio.com/recipes/");
+    $scope.recipes = $firebaseArray(ref);
+  }
 ]);
 
 app.controller("RecipeController", function($scope, $location, $routeParams, $firebaseArray) {
-    $scope.recipes.$loaded().then(function(payload) {
-    $scope.recipe = payload.$getRecord($routeParams.id);})
+  $scope.recipes.$loaded().then(function(payload) {
+    $scope.recipe = payload.$getRecord($routeParams.id);
+  })
 
+  $scope.review = {};
+
+  $scope.addReview = function(recipe) {
+    recipe.reviews.push(this.review);
     $scope.review = {};
+  };
 
-    $scope.addReview = function(recipe) {
-        recipe.reviews.push(this.review);
-        $scope.review = {};
-    };
+  $scope.direction = 'left';
+  $scope.currentIndex = 0;
 
-    $scope.direction = 'left';
-    $scope.currentIndex = 0;
+  $scope.setCurrentSlideIndex = function (index) {
+    $scope.currentIndex = index;
+  };
 
-    $scope.setCurrentSlideIndex = function (index) {
-        $scope.currentIndex = index;
-    };
+  $scope.isCurrentSlideIndex = function (index) {
+    return $scope.currentIndex === index;
+  };
 
-    $scope.isCurrentSlideIndex = function (index) {
-        return $scope.currentIndex === index;
-    };
+  $scope.prevSlide = function () {
+    $scope.currentIndex = ($scope.currentIndex < $scope.recipe.slides.length - 1) ? ++$scope.currentIndex : 0;
+  };
 
-    $scope.prevSlide = function () {
-        $scope.currentIndex = ($scope.currentIndex < $scope.recipe.slides.length - 1) ? ++$scope.currentIndex : 0;
-    };
+  $scope.nextSlide = function () {
+    $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.recipe.slides.length - 1;
+  };
 
-    $scope.nextSlide = function () {
-        $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.recipe.slides.length - 1;
-    };
-
-    $scope.goToEditor = function () {
-      $location.path("/editor/" + $scope.recipe.$id);
-};
+  $scope.goToEditor = function () {
+    $location.path("/editor/" + $scope.recipe.$id);
+  };
 });
 
 app.controller("AddRecipeController", ["$scope", "createRecipes",
   function($scope, createRecipes) {
-    $scope.recipes = createRecipes;
-
-    $scope.addRecipe = function() {
-      $scope.recipes.$add({
-        title: $scope.recipe.title,
-        lead: $scope.recipe.lead,
-        preptime: $scope.recipe.preptime,
-        cookingtime: $scope.recipe.cookingtime,
-        // keywords: $scope.keywords,
-        // ingredients: $scope.ingredients,
-        instructions: $scope.recipe.instructions
-      });
-        
-      alert("Your recipe has been succefully saved!")
-      
-      // reset the recipe input
-      $scope.recipe = "";
+    $scope.initRecipe = function() {
+      // Initialize new recipe (with default values)
+      $scope.recipe = {
+        title: "",
+        lead: "",
+        preptime: "",
+        cookingtime: "",
+        instructions: "",
+        ingredients: [],
+        keywords: []
+      };
     };
 
-    $scope.keywords = [];
-    $scope.ingredients = [];
+    $scope.addRecipe = function() {
+      $scope.recipes = createRecipes;
 
-        $scope.addKeyword = function() {
-          $scope.keywords.push("");
-        };
-        
-        $scope.addIngredient = function() {
-          $scope.ingredients.push("");
-        };
+      $scope.recipes.$add($scope.recipe).then(function() {
+        alert("Your recipe has been succefully saved!")
+      });
+      
+      // reset the recipe input
+      $scope.initRecipe();
+    };
 
-        $scope.removeKeyword = function() {
-          var lastKeyword = $scope.keywords.length-1;
-          $scope.keywords.splice(lastKeyword);
-        };
+    $scope.addKeyword = function() {
+      $scope.recipe.keywords.push("");
+    };
+    
+    $scope.addIngredient = function() {
+      $scope.recipe.ingredients.push("");
+    };
 
-        $scope.removeIngredient = function() {
-          var lastIngredient = $scope.keywords.length-1;
-          $scope.ingredients.splice(lastIngredient);
-        };
-      }
+    $scope.removeKeyword = function() {
+      var lastKeyword = $scope.keywords.length-1;
+      $scope.keywords.splice(lastKeyword);
+    };
+
+    $scope.removeIngredient = function() {
+      var lastIngredient = $scope.ingredients.length-1;
+      $scope.ingredients.splice(lastIngredient);
+    };
+
+    $scope.initRecipe();
+  }
 ]);
 
 app.controller("EditRecipeController", function($scope, $location, $routeParams, $firebaseArray) {
-    $scope.recipes.$loaded().then(function(recipeid) {
-      $scope.recipe = recipeid.$getRecord($routeParams.id);
-    })
+  $scope.recipes.$loaded().then(function(recipeid) {
+    $scope.recipe = recipeid.$getRecord($routeParams.id);
+  })
 
-    $scope.editRecipe = function(){
+  $scope.editRecipe = function(){
     $scope.recipes.$save($scope.recipe).then(function(){
-      console.log($scope.recipe);
-    });
     alert("Your recipe has been succefully updated!")
-}
+    });
+  }
 
   $scope.deleteRecipe = function(){
     $scope.recipes.$remove($scope.recipe).then(function(){
-      console.log($scope.recipe);
-    });
     alert("Your recipe has been succefully deleted!")
+    });
   }
 });
