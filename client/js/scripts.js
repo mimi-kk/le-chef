05cdb780b1b1708cf3dfbdbdc104a851d7b3c663
Nora Rusterholz <norarusterholz@gmail.com>
2015-10-05 21:01:03 +0200

    Added share function

--- client/js/scripts.js
+++ client/js/scripts.js
@@ -1,7 +1,8 @@
 var app = angular.module("leChef", [
     "ngRoute", 
     "ngAnimate",
-    "firebase"
+    "firebase",
+    "720kb.socialshare"
 ]);
 
 app.config(function($routeProvider) {
@@ -26,3 +27,19 @@ app.config(function($routeProvider) {
         redirectTo: "/"
     });
 });
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '774079302702991',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
