423b99db0d13f5be7471562da4592c01b544e0e1
Nora Rusterholz <norarusterholz@gmail.com>
2015-10-22 21:15:19 +0200

    Changed the path to the upload folder in editRecipe function

--- client/js/controllers/recipe-controller.js
+++ client/js/controllers/recipe-controller.js
@@ -105,7 +105,7 @@ app.controller("EditRecipeController", ["$scope", "createRecipes", "$location", 
 
       $scope.editRecipe = function(){
         for(i = 0; i < $scope.upload.flow.files.length; i++) {
-          $scope.recipe.slides.push("/images/"+$scope.upload.flow.files[i].relativePath);
+          $scope.recipe.slides.push("/images/uploads/"+$scope.upload.flow.files[i].relativePath);
         };
 
         $scope.recipes.$save($scope.recipe).then(function(){
