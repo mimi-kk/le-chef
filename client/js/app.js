e2cedd1cdeb0bb31c8c6aeabaacb2c477083b384
Nora Rusterholz <norarusterholz@gmail.com>
2015-10-22 23:10:56 +0200

    Removed Facebook script from app.js

--- client/js/app.js
+++ client/js/app.js
@@ -13,19 +13,3 @@ app.directive("imageSlider", function(){ 
     templateUrl: "templates/slider.html"
 }; 
 });
-
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '774079302702991',
-      xfbml      : true,
-      version    : 'v2.4'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
