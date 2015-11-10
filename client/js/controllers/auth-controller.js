8b66df0b25552ffec69e2b03827019db6c76b64f
Nora Rusterholz <norarusterholz@gmail.com>
2015-11-10 23:42:08 +0100

    Improved login function

--- client/js/controllers/auth-controller.js
+++ client/js/controllers/auth-controller.js
@@ -7,44 +7,19 @@ app.controller("AuthController", ["$scope", "$routeParams", "$firebaseAuth", "Au
     });
 
      $scope.login = function () {
+      $scope.message = null;
+      $scope.error = null;
+
      Auth.$authWithPassword({
      	email: $scope.email,
      	password: $scope.password
-     }, function(error, authData) {
-     	if (error) {console.log("Login Failed!", error);
-     	} 
-     	else {console.log("Authenticated successfully with payload:", authData);
-     }
-     });
+     }).then(function(userData) {
+        $scope.message = "Loged in successfully as " + "" + userData.password.email;
+      }).catch(function(error) {
+        $scope.error = "Something went wrong! Please try again or contact us.";
+      });
     }
 
-    $scope.createUser = function() {
-      $scope.message = null;
-      $scope.error = null;
-
-      Auth.$createUser({
-        email: $scope.email,
-        password: $scope.password
-      }).then(function(userData) {
-        $scope.message = "User created with uid: " + userData.uid;
-      }).catch(function(error) {
-        $scope.error = error;
-      });
-    };
-
-    $scope.removeUser = function() {
-      $scope.message = null;
-      $scope.error = null;
-
-      Auth.$removeUser({
-        email: $scope.email,
-        password: $scope.password
-      }).then(function() {
-        $scope.message = "User removed";
-      }).catch(function(error) {
-        $scope.error = error;
-      });
-    };
 
   }
 ]);
