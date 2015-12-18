caf436e07bbc7579296a8c3bb723f6ab4eee27a4
Nora Rusterholz <norarusterholz@gmail.com>
2015-12-18 16:19:46 +0100

    Updated screen css and js

--- client/dist/screen.js
+++ client/dist/screen.js
@@ -1,1 +1,407 @@
-var app=angular.module("leChef",["ngRoute","ngSanitize","ngAnimate","ngTouch","ngIOS9UIWebViewPatch","ngAria","ckeditor","firebase","720kb.socialshare","flow","angular-spinkit"]);app.config(["flowFactoryProvider",function(e){e.defaults={target:"/images/upload.php",permanentErrors:[500,501],maxChunkRetries:1,chunkRetryInterval:5e3,simultaneousUploads:1},e.on("catchAll",function(e){console.log("catchAll",arguments)})}]),app.run(["$rootScope","$location","$anchorScroll","$routeParams",function(e,r,t,n){e.$on("$routeChangeSuccess",function(e,o){r.hash(n.scrollTo),t()})}]),app.run(["$rootScope","$location",function(e,r){e.$on("$routeChangeError",function(e,t,n,o){"AUTH_REQUIRED"===o&&r.path("/auth")})}]),app.config(["$routeProvider","$compileProvider",function(e,r){r.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/),e.when("/",{templateUrl:"templates/recipes.html",controller:"RecipesController",resolve:{currentAuth:["Auth",function(e){return e.$waitForAuth()}]}}).when("/recipe/:id",{templateUrl:"templates/recipe.html",controller:"RecipeController",resolve:{currentAuth:["Auth",function(e){return e.$waitForAuth()}]}}).when("/editor/",{templateUrl:"templates/editor.html",controller:"EditorController",resolve:{currentAuth:["Auth",function(e){return e.$requireAuth()}]}}).when("/editor/:id",{templateUrl:"templates/editor.html",controller:"EditorController",resolve:{currentAuth:["Auth",function(e){return e.$requireAuth()}]}}).when("/auth",{templateUrl:"templates/auth.html",controller:"AuthController",resolve:{currentAuth:["Auth",function(e){return e.$waitForAuth()}]}}).when("/reset",{templateUrl:"templates/reset.html",controller:"AuthController",resolve:{currentAuth:["Auth",function(e){return e.$waitForAuth()}]}}).when("/new-password",{templateUrl:"templates/new-password.html",controller:"AuthController",resolve:{currentAuth:["Auth",function(e){return e.$waitForAuth()}]}}).otherwise({redirectTo:"/"})}]),app.controller("AuthController",["$scope","$routeParams","$location","$firebaseAuth","Auth",function(e,r,t,n,o){e.auth=o,e.auth.$onAuth(function(r){e.authData=r}),e.login=function(){e.message=null,e.error=null,o.$authWithPassword({email:e.email,password:e.password}).then(function(r){e.message="Loged in successfully as "+r.password.email})["catch"](function(r){e.error="Something went wrong! Please try again or reset password."}).then(function(){null!==e.error?t.path("/auth"):t.path("/")})},e.resetPassword=function(){e.message=null,e.error=null,o.$resetPassword({email:e.email}).then(function(r){null===e.error?e.message="Password reset email sent successfully. Check your inbox.":e.error="Error sending password reset email:"})},e.changePassword=function(){e.message=null,e.error=null,o.$changePassword({email:e.email,oldPassword:e.oldPassword,newPassword:e.newPassword}).then(function(r){null===e.error?e.message="Password changed successfully":e.error="Error changing password:"})}}]),app.controller("EditorController",["$scope","Recipes","$location","$routeParams","currentAuth",function(e,r,t,n,o,i){e.initRecipe=function(){"undefined"==typeof n.id?e.recipe={title:"",lead:"",preptime:"",cookingtime:"",totaltime:"",instructions:"",ingredients:[""],keywords:[""],slides:["/images/dummy-recipe.jpg"]}:e.recipes.$loaded().then(function(r){e.recipe=r.$getRecord(n.id)})},e.upload={},e.options={toolbar:[{items:["Bold","Italic","NumberedList","BulletedList","Link","Unlink"]}]},e.add=function(r){e.recipe[r].push("")},e.remove=function(r,t){e.recipe[r].splice(t,1)},e.addRecipe=function(){for(var n=0;n<e.upload.flow.files.length;n++)e.recipe.slides.push("/images/uploads/"+e.upload.flow.files[n].relativePath);e.recipe.createdOn=Date.now(),e.recipes=r,e.recipes.$add(e.recipe).then(function(){alert("Your recipe has been succefully saved!"),t.path("/")}),e.initRecipe()},e.cancelEditRecipe=function(){alert("If you wish jellyfish!"),e.recipe.$id?t.path("/recipe/"+e.recipe.$id):t.path("/")},e.editRecipe=function(){for(var r=0;r<e.upload.flow.files.length;r++)e.recipe.slides.push("/images/uploads/"+e.upload.flow.files[r].relativePath);e.recipes.$save(e.recipe).then(function(){alert("Your recipe has been succefully updated!"),t.path("/recipe/"+e.recipe.$id)})},e.deleteRecipe=function(){window.confirm("Do you really want to delete your recipe?")&&e.recipes.$remove(e.recipe).then(function(){alert("Your recipe has been succefully deleted!"),t.path("/")})},e.initRecipe()}]),app.controller("RecipeController",["$scope","$location","$routeParams","$compile","Recipes",function(e,r,t,n,o){e.addReview=function(){e.recipe.reviews.push(e.recipe.review),e.recipe.review.createdOn=Date.now(),e.recipe.review={},e.recipes.$save(e.recipe).then(function(){alert("Your review has been succefully sent!"),r.path("/recipe/"+e.recipe.$id)})},e.recipes.$loaded().then(function(r){e.recipe=r.$getRecord(t.id),e.html=e.recipe.instructions,"undefined"==typeof e.recipe.reviews&&(e.recipe.reviews=[{}]),e.calculateAverage=function(r){var t=0;if(e.recipe.reviews.length>1){for(var n=1;n<e.recipe.reviews.length;n++)t+=parseInt(e.recipe.reviews[n].stars,10);var o=t/(e.recipe.reviews.length-1),i=Math.round(o);return{average:i,markedStars:new Array(i)}}return t}}),e.direction="left",e.currentIndex=0,e.setCurrentSlideIndex=function(r){e.currentIndex=r},e.isCurrentSlideIndex=function(r){return e.currentIndex===r},e.prevSlide=function(){e.currentIndex=e.currentIndex<e.recipe.slides.length-1?++e.currentIndex:0},e.nextSlide=function(){e.currentIndex=e.currentIndex>0?--e.currentIndex:e.recipe.slides.length-1},e.goToEditor=function(){r.path("/editor/"+e.recipe.$id)}}]),app.controller("RecipesController",["$scope","$location","Recipes","$http",function(e,r,t,n){e.loading=!0,n.get("https://fiery-inferno-8595.firebaseio.com/recipes/").success(function(e){})["catch"](function(e){})["finally"](function(){e.loading=!1}),e.recipes=t,e.currentPath=window.location}]),app.directive("eventFocus",function(e){return function(r,t,n){t.on(n.eventFocus,function(){e(n.eventFocusId)}),r.$on("$destroy",function(){element.off(n.eventFocus)})}}),app.directive("imageSlider",function(){return{restrict:"E",templateUrl:"templates/slider.html"}}),app.factory("Auth",["$firebaseAuth",function(e){var r=new Firebase("https://fiery-inferno-8595.firebaseio.com/recipes");return e(r)}]),app.factory("Recipes",["$firebaseArray",function(e){var r=new Firebase("https://fiery-inferno-8595.firebaseio.com/recipes/");return e(r)}]),app.factory("focus",function(e,r){return function(t){e(function(){var e=r.document.getElementById(t);e&&e.focus()})}});
+var app = angular.module("leChef", [
+    "ngRoute",
+    "ngSanitize",
+    "ngAnimate",
+    "ngTouch",
+    "ngIOS9UIWebViewPatch",
+    "ngAria",
+    "ckeditor",
+    "firebase",
+    "720kb.socialshare",
+    "flow",
+    "angular-spinkit",
+    "toastr"
+]);
+
+app.config(["flowFactoryProvider", function(flowFactoryProvider) {
+    flowFactoryProvider.defaults = {
+      target: "/images/upload.php",
+      permanentErrors: [500, 501],
+      maxChunkRetries: 1,
+      chunkRetryInterval: 5000,
+      simultaneousUploads: 1
+    };
+    flowFactoryProvider.on("catchAll", function(event) {
+      console.log("catchAll", arguments);
+    });
+  }
+]);
+
+app.config(function(toastrConfig) {
+  angular.extend(toastrConfig, {
+    positionClass: 'toast-bottom-left'
+  });
+});
+
+app.run(["$rootScope", "$location", "$anchorScroll", "$routeParams", 
+  function($rootScope, $location, $anchorScroll, $routeParams) {
+    $rootScope.$on("$routeChangeSuccess", function(newRoute, oldRoute) {
+      $location.hash($routeParams.scrollTo);
+      $anchorScroll();  
+    });
+  }
+]);
+
+app.run(["$rootScope", "$location", 
+  function($rootScope, $location) {
+    $rootScope.$on("$routeChangeError", function(event, next, previous, error) {
+      if (error === "AUTH_REQUIRED") {
+        $location.path("/auth");
+      }
+    });
+  }
+]);
+app.config(["$routeProvider", "$compileProvider", 
+    function($routeProvider, $compileProvider) {
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension):/);
+
+        $routeProvider
+        .when("/", {
+            templateUrl: "templates/recipes.html",
+            controller: "RecipesController",
+            resolve: {
+                "currentAuth": ["Auth", function(Auth) {
+                return Auth.$waitForAuth();
+            }]
+        }})
+        .when("/recipe/:id", {
+            templateUrl: "templates/recipe.html",
+            controller: "RecipeController",
+            resolve: {
+                "currentAuth": ["Auth", function(Auth) {
+                return Auth.$waitForAuth();
+            }]
+        }})
+        .when("/editor/", {
+            templateUrl: "templates/editor.html",
+            controller: "EditorController",
+            resolve: {
+                "currentAuth": ["Auth", function(Auth) {
+                return Auth.$requireAuth();
+            }]
+        }})
+        .when("/editor/:id", {
+            templateUrl: "templates/editor.html",
+            controller: "EditorController",
+            resolve: {
+                "currentAuth": ["Auth", function(Auth) {
+                return Auth.$requireAuth();
+            }]
+        }})
+        .when("/auth", {
+            templateUrl: "templates/auth.html",
+            controller: "AuthController",
+            resolve: {
+                "currentAuth": ["Auth", function(Auth) {
+                return Auth.$waitForAuth();
+            }]
+        }})
+        .when("/reset", {
+            templateUrl: "templates/reset.html",
+            controller: "AuthController",
+            resolve: {
+                "currentAuth": ["Auth", function(Auth) {
+                return Auth.$waitForAuth();
+            }]
+        }})
+        .when("/new-password", {
+            templateUrl: "templates/new-password.html",
+            controller: "AuthController",
+            resolve: {
+                "currentAuth": ["Auth", function(Auth) {
+                return Auth.$waitForAuth();
+            }]
+        }})
+        .otherwise({
+            redirectTo: "/"
+        });
+    }
+]);
+app.controller("AuthController", ["$scope", "$routeParams", "$location", "$firebaseAuth", "Auth",
+  function($scope, $routeParams, $location, $firebaseAuth, Auth) {
+    $scope.auth = Auth;
+
+    $scope.auth.$onAuth(function(authData) {
+      $scope.authData = authData;
+    });
+
+    $scope.login = function() {
+      $scope.message = null;
+      $scope.error = null;
+
+      Auth
+      .$authWithPassword({
+       	email: $scope.email,
+       	password: $scope.password})
+      .then(function(userData) {
+        $scope.message = "Loged in successfully as " + "" + userData.password.email;})
+      .catch(function(error) {
+        $scope.error = "Something went wrong! Please try again or reset password.";})
+      .then(function() {
+        if ($scope.error !== null) {
+          $location.path("/auth");
+        }
+        else {
+          $location.path("/");
+        }
+      });
+    };
+
+    $scope.resetPassword = function() {
+      $scope.message = null;
+      $scope.error = null;
+
+      Auth
+      .$resetPassword({
+        email: $scope.email})
+      .then(function(error) {
+        if ($scope.error === null) {
+          $scope.message = "Password reset email sent successfully. Check your inbox.";
+        } else {
+          $scope.error = "Error sending password reset email:";
+        }
+      });
+    };
+
+    $scope.changePassword = function() {
+      $scope.message = null;
+      $scope.error = null;
+
+      Auth
+      .$changePassword({
+        email: $scope.email,
+        oldPassword: $scope.oldPassword,
+        newPassword: $scope.newPassword})
+      .then(function(error) {
+        if ($scope.error === null) {
+          $scope.message = "Password changed successfully";
+        } else {
+          $scope.error = "Error changing password:";
+        }
+      });
+    };
+  }
+]);
+app.controller("EditorController", ["$scope", "toastr", "Recipes", "$location", "$routeParams", "currentAuth",
+  function($scope, toastr, Recipes, $location, $routeParams, currentAuth, focus) {
+    $scope.initRecipe = function() {
+      // Initialize new recipe (with default values)
+      if (typeof $routeParams.id === "undefined") {
+        $scope.recipe = {
+          title: "",
+          lead: "",
+          preptime: "",
+          cookingtime: "",
+          totaltime: "",
+          instructions: "",
+          ingredients: [""],
+          keywords: [""],
+          slides: ["/images/dummy-recipe.jpg"]
+        };
+      } else {
+          $scope.recipes.$loaded().then(function(recipeid) {
+            $scope.recipe = recipeid.$getRecord($routeParams.id);
+          });
+        };
+      }
+
+      $scope.upload = {};
+
+      /*CK Editor Options*/
+      $scope.options = {
+        toolbar: [{ 
+          items: [
+            "Bold",
+            "Italic",
+            "NumberedList",
+            "BulletedList",
+            "Link",
+            "Unlink"
+          ]
+        }]
+      };
+
+      $scope.add = function(key) {
+        $scope.recipe[key].push("");
+      };
+
+      $scope.remove = function(key, index) {
+        $scope.recipe[key].splice(index, 1);
+      };
+
+      $scope.addRecipe = function() {
+        for(var i = 0; i < $scope.upload.flow.files.length; i++) {
+          $scope.recipe.slides.push("/images/uploads/"+$scope.upload.flow.files[i].relativePath);
+        };
+
+        $scope.recipe.createdOn = Date.now();
+
+        $scope.recipes = Recipes;
+
+        $scope.recipes.$add($scope.recipe).then(function() {
+          toastr.success("Your recipe has been succefully saved!");
+          $location.path("/");
+        });
+        
+        // reset the recipe input
+        $scope.initRecipe();
+      };
+
+      $scope.cancelEditRecipe = function() {
+        toastr.success("If you wish jellyfish!");
+        if ($scope.recipe.$id) {
+          $location.path("/recipe/" + $scope.recipe.$id);
+        } else {
+          $location.path("/");
+        }
+      };
+
+      $scope.editRecipe = function() {
+        for (var i = 0; i < $scope.upload.flow.files.length; i++) {
+          $scope.recipe.slides.push("/images/uploads/"+$scope.upload.flow.files[i].relativePath);
+        }
+
+        $scope.recipes.$save($scope.recipe).then(function() {
+          toastr.success("Your recipe has been succefully updated!");
+          $location.path("/recipe/" + $scope.recipe.$id);
+        });
+      };
+
+      $scope.deleteRecipe = function(){
+        if (window.confirm("Do you really want to delete your recipe?")){
+          $scope.recipes.$remove($scope.recipe).then(function(){
+            toastr.success("Your recipe has been succefully deleted!");
+            $location.path("/");
+          });
+        }
+      };
+
+      $scope.initRecipe();
+    }
+]);
+app.controller("RecipeController", ["$scope", "toastr", "$location", "$routeParams", "$compile", "Recipes",
+  function($scope, toastr, $location, $routeParams, $compile, Recipes) {
+    $scope.addReview = function() {
+      $scope.recipe.reviews.push($scope.recipe.review);
+      $scope.recipe.review.createdOn = Date.now();
+      $scope.recipe.review = {};
+
+      $scope.recipes.$save($scope.recipe).then(function(){
+        $scope.reviewForm.$setPristine();
+        toastr.success("Your review has been succefully sent!");
+        $location.path("/recipe/" + $scope.recipe.$id);
+      });
+    };
+
+    $scope.recipes.$loaded().then(function(payload) {
+      $scope.recipe = payload.$getRecord($routeParams.id);
+      
+      $scope.html = $scope.recipe.instructions;
+
+      if (typeof $scope.recipe.reviews === "undefined") {
+        $scope.recipe.reviews = [{}];
+      }
+
+      $scope.calculateAverage = function(AverageData){
+        var sum = 0;
+        
+        if ($scope.recipe.reviews.length > 1) {
+          for(var i = 1; i < $scope.recipe.reviews.length; i++){
+            sum += parseInt($scope.recipe.reviews[i].stars, 10);
+          }
+
+          var average = sum/($scope.recipe.reviews.length-1);
+          var roundedAverage = Math.round(average);
+          return { 
+            average: roundedAverage,
+            markedStars: new Array(roundedAverage)
+          };
+        } else {
+          return sum;
+        }
+      };
+    });
+
+    $scope.direction = "left";
+    $scope.currentIndex = 0;
+
+    $scope.setCurrentSlideIndex = function(index) {
+      $scope.currentIndex = index;
+    };
+
+    $scope.isCurrentSlideIndex = function(index) {
+      return $scope.currentIndex === index;
+    };
+
+    $scope.prevSlide = function() {
+      $scope.currentIndex = ($scope.currentIndex < $scope.recipe.slides.length - 1) ? ++$scope.currentIndex : 0;
+    };
+
+    $scope.nextSlide = function() {
+      $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.recipe.slides.length - 1;
+    };
+
+    $scope.goToEditor = function() {
+      $location.path("/editor/" + $scope.recipe.$id);
+    };
+  }
+]);
+app.controller("RecipesController", ["$scope", "$location", "Recipes", "$http",
+  function($scope, $location, Recipes, $http) {
+  	$scope.loading = true;
+  	
+    $http.get("https://fiery-inferno-8595.firebaseio.com/recipes/")
+      .success(function(data) {})
+      .catch(function(err) {})
+      .finally(function() {
+        $scope.loading = false;
+      });
+  	
+    $scope.recipes = Recipes;
+    $scope.currentPath = window.location;
+  }]
+);
+app.directive("eventFocus", function(focus) {
+    return function(scope, elem, attr) {
+        elem.on(attr.eventFocus, function() {
+        focus(attr.eventFocusId);
+    });
+
+    // Removes bound events in the element itself
+    // when the scope is destroyed
+    scope.$on("$destroy", function() {
+            element.off(attr.eventFocus);
+        });
+    };
+})
+app.directive("imageSlider", function(){ 
+    return {
+        restrict: "E",
+        templateUrl: "templates/slider.html"
+    }; 
+});
+app.factory("Auth", ["$firebaseAuth",
+  function($firebaseAuth) {
+    var ref = new Firebase("https://fiery-inferno-8595.firebaseio.com/recipes");
+    return $firebaseAuth(ref);
+  }
+]);
+app.factory("Recipes", ["$firebaseArray",
+  function($firebaseArray) {
+    var ref = new Firebase("https://fiery-inferno-8595.firebaseio.com/recipes/");
+    return $firebaseArray(ref);
+  }
+]);
+app.factory("focus", function($timeout, $window) {
+  return function(id) {
+  // timeout makes sure that is invoked after any other event has been triggered.
+  // e.g. click events that need to run before the focus or
+  // inputs elements that are in a disabled state but are enabled when those events
+  // are triggered.
+    $timeout(function() {
+      var element = $window.document.getElementById(id);
+      if(element)
+      element.focus();
+    });
+  };
+})
